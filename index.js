2efbc2816e5a6995ddb35e70754261364080de25
Elise Phan <aliceconnic@gmail.com>
2019-03-14 04:05:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ function driversWithRevenueOver(drivers, revenue) {
 function driverNamesWithRevenueOver(drivers, revenue) {
   return driversWithRevenueOver(drivers, revenue).map(function (driver) {
     return driver.name
-  }) {
-    return 
-  })
+  });
 }
 
 function exactMatch() {
