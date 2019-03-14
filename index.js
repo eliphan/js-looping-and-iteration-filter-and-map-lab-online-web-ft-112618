8bf7bd91019bbed6cd7e79a28392610b9dc8f563
Elise Phan <aliceconnic@gmail.com>
2019-03-14 04:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-  drivers.map(function (driver) {
+  drivers.filter(function (driver) {
     return driver.revenue > revenue
   })
 }
